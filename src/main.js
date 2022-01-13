12d957a5e12d665ef26bd99e0e33fb4e35985126
Akira <9316187+sawaakira@user.noreply.gitee.com>
2022-01-13 09:21:18 +0800

    1月13日提交代码

--- src/main.js
+++ src/main.js
@@ -7,6 +7,19 @@ import './assets/animate/css/animate.min.css'
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 import lottie from 'vue-lottie'
 
+let initRoot = () => {
+  let root = document.documentElement;
+  let winWidth = document.documentElement.clientWidth || window.innerWidth;
+  if(winWidth > 375 ){
+     winWidth = 375;
+  }
+  //  375 / 10 = 37.5
+  //  1rem = 37.5 px
+  root.style['fontSize'] = winWidth / 375 * 37.5 + "px";
+}
+initRoot()
+window.addEventListener('resize',initRoot);
+
 Vue.config.productionTip = false
 Vue.component('lottie', lottie)
 
